8cb1f15705deb5a8ca122a836c3facee2c44033f
rinshaddev <rinshadr843@gmail.com>
2023-01-25 11:51:19 +0530

    Add event listening
    add event listener to the color picker change the background of the pen color button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-window.onload=()=> {defaultGrid();}
+
 let currentColor = '#000000';
+let currentSize = 16;
 
 const rangeValue = document.getElementById('show-range-value');
 const gridContainer = document.getElementById('canvas');
@@ -9,30 +10,45 @@ const btnColorPicker = document.getElementById('btn-color-picker');
 const btnRainbow = document.getElementById('btn-random-color');
 const btnClear = document.getElementById('btn-clear');
 const btnEraser = document.getElementById('btn-eraser');
+const slider  = document.getElementById('range-collector');
 
 const colorPicker = document.getElementById('color-picker');
-const grid = document.querySelectorAll('.grid')
 
 
+colorPicker.addEventListener('change',  hello);
 
+ function hello (e){
+    currentColor = e.target.value;
+    setBackgroundColor();
 
-function defaultGrid(){
-    gridContainer.style.gridTemplateColumns= 'repeat(16, 1fr)';
-    gridContainer.style.gridAutoRows = 'repeat (16, 1fr)'
-     for (let i = 0; i < 16 * 16; i++) {
-        let grid = document.createElement("div");
-        grid.classList.add('grid');
-        grid.addEventListener('mouseenter',() => {grid.style.backgroundColor = currentColor})
-        gridContainer.appendChild(grid)
-    }
+ }
 
-}
 
 
 
  
+function setUpSpecificColorGrid(e){
+   gridContainer.style.gridTemplateColumns= `repeat(${currentSize}, 1fr)`;
+   gridContainer.style.gridAutoRows = `repeat (${currentSize}, 1fr)`
+     for (let i = 0; i < currentSize * currentSize; i++) {
+      let grid = document.createElement("div");
+        grid.classList.add('grid');
+        grid.addEventListener('mouseenter',() => {grid.style.backgroundColor = currentColor})
+        gridContainer.appendChild(grid)
+    }
+   
+
+
+}
+function setBackgroundColor() {
+    btnColorPicker.style.background = currentColor;
+    btnColorPicker.style.color = 'white'
+}
+
+window.onload =  () =>{
+setBackgroundColor()
+setUpSpecificColorGrid()
+}
 
 
 
-  
-    
