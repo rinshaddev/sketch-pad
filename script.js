891c1be13d229277da07fa6c6b060f6d08105b8e
rinshaddev <rinshadr843@gmail.com>
2023-01-30 15:36:44 +0530

    Display color picker and cursor
    when the palette button clicked display the colorpicker input and change palette color after change the color
    to show cursor when mouse enter the canvas and shows the cusor acordding to the button clikced

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 
+const DEFAULT_COLOR = "normal";
+
 let currentColor = '#000000';
 let currentSize = 16;
+let currentType = DEFAULT_COLOR;
+
+
 
 const rangeValue = document.getElementById('show-range-value');
 const gridContainer = document.getElementById('canvas');
+const brushContainer = document.getAnimations('btn-color-picker-txt');
+const brushColor = document.querySelector('.fa-solid.fa-paintbrush');
+const palette = document.querySelector('.fa-solid.fa-palette');
 
 
 const btnColorPicker = document.getElementById('btn-color-picker');
@@ -12,80 +20,81 @@ const btnClear = document.getElementById('btn-clear');
 const btnEraser = document.getElementById('btn-eraser');
 const slider  = document.getElementById('range-collector');
 
+
 const colorPicker = document.getElementById('color-picker');
 
 
-colorPicker.addEventListener('change',  setColorBtnGrid);
-btnRainbow.addEventListener('click', rainbow);
-// btnColorPicker.addEventListener('click', )
 
-function rainbow (e){
-   grids.forEach(element => {
-    element.removeEventListener ('mouseenter', changeColor)
-   });
-   grids.forEach(element => {
-    element.addEventListener ('mouseenter', hello)
-   })
-  function hello(e){
-    e.target.style.backgroundColor =`#${Math.floor(Math.random()*16777215)}`
-   }}
-
-function normal (e){
-
- 
- }
+btnColorPicker.addEventListener('click',setUpBtnAndColorPicker);
+colorPicker.addEventListener('change', setUpBtnAndColorPicker);
+gridContainer.addEventListener ("mouseenter", showingCursor);
+gridContainer.addEventListener ('mouseleave',  showingCursor);
+gridContainer.addEventListener("mousemove", curserShow);
 
 
-function colorGenerator(){
-  return 
-}
-
-
-
- function setColorBtnGrid (e){
-    currentColor = e.target.value;
-    setBackgroundColor();
-    // to reassign the current with the value it's currently rainbow; 
-   grids.forEach(element => {
-     element.addEventListener ('mouseenter', hi)
-    })
-    function hi(e){
-     e.target.style.backgroundColor = currentColor;
-    }
-
- }
-
-
-
-let grids = [...document.querySelectorAll('.grid')]
-console.log(grids)
- 
-function setUpSpecificColorGrid(e){
-   gridContainer.style.gridTemplateColumns= `repeat(${currentSize}, 1fr)`;
-   gridContainer.style.gridAutoRows = `repeat (${currentSize}, 1fr)`;
-  
-     for (let i = 0; i < currentSize * currentSize; i++) {
-      let grid = document.createElement("div");
-        grid.classList.add('grid');
-        grid.addEventListener('mouseenter',changeColor)
-        grids.unshift(grid)
-        gridContainer.appendChild(grid)
-    }
+//To display the custom cursor when enter the container
+function showingCursor (e){
+    let brushCursor = document.getElementById ('brush-cursor')
+    
+    
+    //To whether mouse enter or not to show the cursor
+    if (e.type == 'mouseenter'){ 
+        brushCursor.style.display ="block"}
+        else if (e.type == 'mouseleave'){
+            brushCursor.style.display = "none"
+        }
     
 }
-function setBackgroundColor() {
-    btnColorPicker.style.background = currentColor;
-    btnColorPicker.style.color = 'white'
-}
-
-window.onload =  () =>{
-setBackgroundColor()
-setUpSpecificColorGrid()
-}
 
 
-
-function changeColor(e){
-        e.target.style.background = currentColor;
-}   
+// to display color picker
+function  setUpBtnAndColorPicker(event) {
    
+   displayColorPickerAndPallette(event);
+} 
+
+
+function displayColorPickerAndPallette (event){
+   if (event.type == 'change'){
+    colorPicker.style.display = 'none';
+    btnColorPicker.style.display = 'block';
+    //change color of palette and brush;
+    ColorPaletteBrush(event.target.value);
+   }// to show the color picker input when the button is clicked
+   else if (event.type == 'click'){
+    btnColorPicker.style.display = 'none';
+    colorPicker.style.display = 'block';
+    
+   }
+
+}
+    
+
+
+// to set the color of brush and palette
+function ColorPaletteBrush(event) {
+   palette.style.color = event;
+   brushColor.style.color = 'black';
+   currentColor = event;
+}
+
+  
+
+//show the according to the mouse mouse move
+function curserShow(e) {
+   const brushCursor = document.getElementById ('brush-cursor');
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    brushCursor.style.position ="absolute"
+    brushCursor.style.left = `${mouseX}px`
+    brushCursor.style.top = `${mouseY}px`
+    brushCursor.style.zIndex = 11;
+    
+}
+
+
+
+window.onload = () => {
+    setUpGrid();
+    ColorPaletteBrush(currentColor);
+}
